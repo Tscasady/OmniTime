d579988d5dd4b6a4ff679e993ad901939f8ef98b
Thomas Casady <tscasady@gmail.com>
2023-05-18 12:40:17 -0500

    Add incrementing button.

---
+++ src/routes/Counter.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+  let count = 0;
+
+  function handleClick() {
+    return count += 1
+  }
+
+  export let text: string;
+</script>
+
+<button on:click={handleClick}>
+  {text} {count}   
+</button>
+
+<style>
+  button {
+    color: gainsboro;
+    background-color: sienna;
+    border: 1px solid blue;
+    border-radius: .35rem;
+    padding: 1rem;
+  }
+</style>
