e1a529aba845c00e97a351bf0be8f0fbdf351fe5
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-05-19 12:23:42 -0700

    Add timer display fixes and functionality to minutes and seconds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
-<script lang='ts'>
-  import Counter from './Counter.svelte';
-  import TimerContainer from './TimerContainer.svelte';
-  let count: number; 
+<script lang="ts">
+	import Counter from './Counter.svelte';
+	import TimerContainer from './TimerContainer.svelte';
+	let count: number;
 
-  function handleMessage(event){
-    count = event.detail.count     
-  }
-  
+	function handleMessage(event) {
+		count = event.detail.count;
+	}
 </script>
+
 <h1>Welcome to OmniTime</h1>
-<Counter on:message={handleMessage} text="Select number of players" count={count}/>
-<TimerContainer numberOfPlayers={count}/>
+<Counter on:message={handleMessage} text="Select number of players" {count} />
+<TimerContainer numberOfPlayers={count} />
--- src/routes/Timer.svelte
+++ src/routes/Timer.svelte
@@ -1,55 +1,58 @@
-<script lang='ts'>
+<script lang="ts">
+	export let duration: number;
+	let timeElapsed = 0;
+	let timeRemaining = 0;
+	let seconds = duration % 60;
+	let minutes = duration / 60;
+	$: timeRemaining = duration - timeElapsed;
+	$: seconds = timeRemaining % 60;
+	$: minutes = Math.floor(timeRemaining / 60);
+	let appendSeconds = '0' + seconds.toString();
+	let appendMinutes = minutes.toString();
 
-  export let duration: number;
-  let timeElapsed = 0; 
-  let timeRemaining = 0;
-  let seconds = 0;
-  let minutes = 0;
-  $: timeRemaining = duration - timeElapsed;
-  $: seconds = 60 - timeRemaining % 60 
-  $: minutes = 60 - timeRemaining / 60 
-  let appendSeconds = "0" + seconds.toString(); 
-  let appendMinutes = "0" + minutes.toString(); 
-  
-  function start(){
-    setInterval(startTimer, 1000);
-  }
+	function start() {
+		startTimer();
+		setInterval(startTimer, 1000);
+	}
 
-  function reset() {
-    seconds = 0;
-    appendSeconds = seconds.toString();
-  }
-   
-  
-  function startTimer() {
-    timeRemaining++;
+	async function incrementTime() {
+		timeElapsed++;
+	}
 
-    if (seconds < 10) {
-      appendSeconds = "0" + seconds;
-    } else {
-      appendSeconds = seconds.toString();
-    }
-  }
+	function startTimer() {
+		incrementTime();
+		setTimeout(formatTime, 0);
+	}
 
-  export let id: number;
+	function formatTime() {
+		if (seconds < 10) {
+			appendSeconds = '0' + seconds;
+		} else {
+			appendSeconds = seconds.toString();
+		}
 
-  function pause(){
-         
-  } 
-  
-  function disable(){
+		if (minutes < 10) {
+			appendMinutes = '0' + minutes;
+		} else {
+			appendMinutes = minutes.toString();
+		}
+	}
 
-  }
-  
+	function reset() {
+		seconds = 0;
+		appendSeconds = seconds.toString();
+	}
+
+	export let id: number;
+
+	function pause() {}
+
+	function disable() {}
 </script>
 
 <div>
-  <p>I'm a timer with time: {appendMinutes}:{appendSeconds}</p>  
-  <button on:click={start}>
-    START
-  </button>
+	<p>I'm a timer with time: {appendMinutes}:{appendSeconds}</p>
+	<button on:click={start}> START </button>
 </div>
 
 <style></style>
-
-
